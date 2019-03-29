82dd4991448d08c3472fe6418489fd0042b8bc09
Christos Nasikas <xristosnasikas@gmail.com>
2019-03-29 17:40:53 +0200

    Start SCALE players

--- player.js
+++ player.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const _ = require('lodash')
 const WebSocket = require('ws')
 const Player = require('./Player')
-const { print } = require('./helpers')
+const { print, pack, unpack } = require('./helpers')
 
 const PORT = process.env.PORT || 3003
 
@@ -21,17 +21,18 @@ const player = new Player(process.env.ID)
 wss.on('connection', (ws) => {
   print('Connection Accepted!')
 
-  ws.on('message', (message) => {
-    print(`Message: ${message}`)
-    if (message === 'start') {
+  ws.on('message', (data) => {
+    print(`Message: ${data}`)
+    data = unpack(data)
+    if (data.message === 'start') {
       player.run()
 
       player.on('listen', () => {
-        ws.send('listen')
+        ws.send(pack({ message: 'listen', player: { id: player.id } }))
       })
 
       player.on('exit', () => {
-        ws.send('exit')
+        ws.send(pack({ message: 'exit', player: { id: player.id } }))
       })
     }
   })
