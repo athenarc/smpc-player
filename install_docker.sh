e74ff7886f421aa62d8e2490822e4b8baac23160
Christos Nasikas <xristosnasikas@gmail.com>
2019-05-20 15:59:34 +0300

    Run compose with sudo

--- install_docker.sh
+++ install_docker.sh
@@ -24,7 +24,7 @@ sudo docker run hello-world
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+sudo docker-compose --version
 
 mkdir -p certs
 mkdir -p scale/data
