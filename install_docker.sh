46e81d396b673f5212f7569e48069b407eddb3b5
Christos Nasikas <xristosnasikas@gmail.com>
2019-05-20 15:58:50 +0300

    Enforce installation

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -15,7 +15,7 @@ sudo add-apt-repository \
    stable"
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 sudo usermod -aG docker $USER
 
