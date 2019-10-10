028be1a3fb15c3f9171b07c00b748e8512565c83
Christos Nasikas <xristosnasikas@gmail.com>
2019-10-10 13:15:20 +0300

    Refactor to protocols

--- player.js
+++ player.js
@@ -4,8 +4,8 @@ const _ = require('lodash')
 const https = require('https')
 const fs = require('fs')
 const WebSocket = require('ws')
-const Player = require('./PlayerSMPC')
-const { print, pack, unpack } = require('./helpers')
+const SMPCProtocol = require('./protocols/SMPC')
+const { print } = require('./helpers')
 
 const PORT = process.env.PORT || 3003
 
@@ -43,62 +43,9 @@ const wss = new WebSocket.Server({ server })
 
 console.log(`Player ${process.env.ID} started on port ${PORT}.`)
 
-const handleConnection = (ws) => {
-  const player = new Player(process.env.ID)
-
-  player.on('compilation-ended', (msg) => {
-    ws.send(pack({ message: 'compilation-ended', ...msg }))
-  })
-
-  player.on('listen', () => {
-    ws.send(pack({ message: 'listen', player: { id: player.id } }))
-  })
-
-  player.on('error', (msg) => {
-    ws.send(pack({ message: 'error', ...msg }))
-  })
-
-  player.on('exit', (msg) => {
-    ws.send(pack({ message: 'exit', entity: 'player', ...msg }))
-  })
-
-  ws.on('close', (data) => {
-    console.log('Connection Closed: ', data)
-    player.terminate()
-    player.removeAllListeners()
-  })
-
-  ws.on('error', (err) => {
-    console.log(err)
-    player.terminate()
-    player.removeAllListeners()
-  })
-
-  ws.on('message', (data) => {
-    print(`Message: ${data}`)
-    data = unpack(data)
-
-    if (data.message === 'job-info') {
-      player.setJob({ ...data.job })
-    }
-
-    if (data.message === 'compile') {
-      player.compileProgram({ ...data.dataInfo, clients: data.job.totalClients })
-    }
-
-    if (data.message === 'start') {
-      player.run()
-    }
-
-    if (data.message === 'restart') {
-      player.terminate()
-    }
-  })
-}
-
 wss.on('connection', (ws) => {
   print('Connection Accepted!')
-  handleConnection(ws)
+  const client = new SMPCProtocol({ ws })
 })
 
 server.listen(process.env.PORT)
